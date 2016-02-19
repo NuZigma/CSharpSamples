2eeaf7c87be088913c34ff98bbb66aace785f530
nitrix-reloaded <nithinmohan_tk@optum.com>
2016-02-19 11:16:13 +0530

    minor changes

---
+++ DotNetConsoleApp/DotNetConsoleApp/Sample/TheFrog.cs
@@ -0,0 +1,21 @@
+﻿//
+//  TheFrog.cs
+//
+//
+//  Author:
+//       Nithin Mohan (nitrix-reloaded)
+//
+//  Copyright (c) 2016 - NitRiX-Reloaded
+//
+using System;
+
+namespace DotNetConsoleApp
+{
+	public class TheFrog
+	{
+		public TheFrog ()
+		{
+		}
+	}
+}
+
